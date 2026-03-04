d49e2d222adbbc09c91da8e8f57bbc80b5aa7acc
Jane <evalnaumenko99@gmail.com>
2026-03-04 14:14:51 +0300

    Добавлено имя для поля ввода номера телефона в форме заказа; реализована логика удаления товара из корзины при нажатии кнопки минус, если количество товара равно 1; создан новый файл для управления статусом корзины.

--- js/counter-02.js
+++ js/counter-02.js
@@ -18,15 +18,22 @@ window.addEventListener('click', function(event) {
        
     // Проверяем является ли элемент кнопка плюс или минус
     if (action === 'plus' || action === 'minus') {
-        
+
         // Обновляем значение счетчика
         // Проверяем нажата ли кнопка МИНУС
         if (action === 'minus') {
+
+              // проверка на товар внутри корзины, чтобы удалить товар
+            if (event.target.closest('.cart-wrapper') && currentValue === 1) {
+                event.target.closest('.cart-item').remove();
+            }
             // Проверяем чтобы счётчик был больше 1
             if (currentValue > 1) {
                 // Уменьшаем значение на 1
                 counterElement.innerText = currentValue - 1;
-            }
+            } else if (event.target.closest('.cart-wrapper') && currentValue === 1) {
+                event.target.closest('.cart-item').remove();  // проверка на товар внутри корзины, чтобы удалить товар
+            } 
         } 
         // Проверяем нажата ли кнопка ПЛЮС
         else if (action === 'plus') {
